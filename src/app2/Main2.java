bc24a0fbbce8775e2d1ef550320348645984c3e2
Stéphanie <stephanie.martial@live.fr>
2022-06-01 17:09:33 +0200

    premiers exos java

---
+++ src/app2/Main2.java
@@ -0,0 +1,97 @@
+package app2;
+
+import java.sql.Date;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+
+//		System.out.println("Hello world2");
+//		
+//		Scanner scanner = new Scanner(System.in);
+//				
+//			System.out.println("Ecrivez un nombre svp");
+//		try  {
+//			
+//			int nombre = scanner.nextInt();	
+//			System.out.println(nombre);
+//		}catch(InputMismatchException e) {
+//			System.out.println("Veuillez entrer un nombre svp!");
+//		}
+//		
+//		scanner.close();
+
+		System.out.println("-------------------------------------------");
+
+//		for (int index = 0; index < 20; index++) {
+//			if (index % 2 == 0) {
+//				System.out.println(index);
+//			}
+//		}
+
+//Ecrire un programme java qui demande à l’utilisateur de saisir 
+//un nombre entier et de lui afficher que le nombre est pair ou 
+//impair selon la valeur tapée		
+
+//		Scanner scanUser = new Scanner(System.in);
+//
+//		System.out.println("Ecrivez un nombre entier");
+//
+//		try {
+//
+//			int number = scanUser.nextInt();
+//
+//			System.out.println(number);
+//
+//			if (number % 2 == 0) {
+//				System.out.println("le nombre entré est pair");
+//			} else {
+//				System.out.println("le nombre entré est impair");
+//			}
+//		} catch (Exception e) {
+//			System.out.println("Vous devez entrer un nombre entier");
+//
+//		}
+//		scanUser.close();
+//		
+//Demandez trois nombres de l'utilisateur et affichez le plus grand nombre.
+
+//		Scanner scanUserNumber1 = new Scanner(System.in);
+//
+//		System.out.println("Votre premier nombre");
+//
+//		try {
+//			int number1 = scanUserNumber1.nextInt();
+//
+//			Scanner scanUserNumber2 = new Scanner(System.in);
+//
+//			System.out.println("Votre deuxième nombre");
+//
+//			int number2 = scanUserNumber2.nextInt();
+//
+//			Scanner scanUserNumber3 = new Scanner(System.in);
+//
+//			System.out.println("Votre troisième nombre");
+//
+//			int number3 = scanUserNumber3.nextInt();
+//
+//			if (number1 > number2 && number1 > number3) {
+//				System.out.println(number1 + " " + "est le plus grand nombre");
+//				scanUserNumber1.close();
+//			} else if (number2 > number1 && number2 > number3) {
+//				System.out.println(number2 + " " + "est le plus grand nombre");
+//				scanUserNumber2.close();
+//			} else
+//				System.out.println(number3 + " " + "est le plus grand nombre");
+//			scanUserNumber3.close();
+//		} catch (Exception e) {
+//			System.out.println("Valeur incorrecte");
+//		}
+
+//Écrire un programme Java pour convertir des minutes en un certain nombre d'années et de jours
+
+	}
+}
