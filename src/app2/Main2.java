1e8159e18d447625aaaecf190dafd7d79ba866ff
Stéphanie <stephanie.martial@live.fr>
2022-06-01 22:38:30 +0200

    jour 1 notions de base de Java + exercices complets

--- src/app2/Main2.java
+++ src/app2/Main2.java
@@ -1,9 +1,7 @@
 package app2;
 
-import java.sql.Date;
-import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
+import java.util.InputMismatchException;
 
 public class Main2 {
 
@@ -25,16 +23,18 @@ public class Main2 {
 //		scanner.close();
 
 		System.out.println("-------------------------------------------");
-
-//		for (int index = 0; index < 20; index++) {
+		
+// Ecrire un code Java qui permet d’afficher les nombres pairs compris entre 0
+// et 10. (value % 2 == 0)
+//		for (int index = 0; index < 11; index++) {
 //			if (index % 2 == 0) {
 //				System.out.println(index);
 //			}
 //		}
 
-//Ecrire un programme java qui demande à l’utilisateur de saisir 
-//un nombre entier et de lui afficher que le nombre est pair ou 
-//impair selon la valeur tapée		
+// Ecrire un programme java qui demande à l’utilisateur de saisir 
+// un nombre entier et de lui afficher que le nombre est pair ou 
+// impair selon la valeur tapée		
 
 //		Scanner scanUser = new Scanner(System.in);
 //
@@ -92,6 +92,38 @@ public class Main2 {
 //		}
 
 //Écrire un programme Java pour convertir des minutes en un certain nombre d'années et de jours
-
+		// le nombre de minutes dans une année = 60 minutes * 24 heures * 365 jours
+//		double minutesInYear = 60 * 24 * 365;
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("Input the number of minutes: ");
+//		double min = input.nextDouble();
+//		long years = (long) (min / minutesInYear);
+//		int days = (int) (min / 60 / 24) % 365;
+//		System.out.println((int) min + " minutes is approximately " + years + " years and " + days + " days");
+//		input.close();
+		
+	
+		// Ecrire un programme pour que pour un cours du dollar donné (et qui ne change
+		// pas pendant l’exécution du programme),
+		// on puisse convertir en euro.
+		// Prévoyez un moyen d’arrêter l’exécution du programme.
+//		double euros, dollar;
+//		double cours;
+//		String choix;
+//		Scanner in = new Scanner(System.in);
+//		try {
+//			do {
+//				System.out.println("Cours du dollar(valeur de 1 dollar)?");
+//				cours = in.nextDouble();
+//				System.out.println("Somme en dollar ?");
+//				dollar = in.nextDouble();
+//				euros = dollar * cours;
+//				System.out.println("La somme en euros:" + euros);
+//				System.out.println("Voulez-vous quitter" + "(tapez q pour quitter)");
+//				choix = in.next();
+//			} while (choix == "q");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 }
